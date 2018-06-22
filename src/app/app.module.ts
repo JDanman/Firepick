5ace8f37ab29e9f3164e6d6e8d6628505a97a102
Jesus Daniel Perez Garcia <perezg.jdaniel@outlook.com>
2018-06-22 10:59:24 -0500

    Git Inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { environment } from '../environments/environment';
+
+import { AngularFireModule } from 'angularfire2';
 
 @NgModule({
   declarations: [
@@ -14,7 +17,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDuGXtLkeMTXm8tWrxBSxj-WgyQn0H0BSY",
+        authDomain: "firepick-f33ec.firebaseapp.com",
+        databaseURL: "https://firepick-f33ec.firebaseio.com",
+        projectId: "firepick-f33ec",
+        storageBucket: "firepick-f33ec.appspot.com",
+        messagingSenderId: "35204321781"
+    }
+};
